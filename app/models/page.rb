d97f3c7d52f1a3e0d20e2bbba2871d8023eee71b
Thirkellian <thirkellian@gmail.com>
2015-10-21 10:54:44 -0500

    Add validations to page model.

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,2 +1,7 @@
 class Page < ActiveRecord::Base
+  attr_accessible :title, :slug, :blurb,  :content
+  validates :title, presence: true, length: { minimum: 5 }
+  validates :slug, presence: true, format: { with: /\A[a-zA-Z-]+\z/}
+  validates :content, presence: true, length: { minimum: 10 }
+
 end
