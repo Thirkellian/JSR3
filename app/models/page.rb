d14be14dcfeddbde7709c8a48a831290b1cd318d
Thirkellian <thirkellian@gmail.com>
2015-10-21 11:31:27 -0500

    Add tests for index, page with id, and about page and make them pass.

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,5 +1,5 @@
 class Page < ActiveRecord::Base
-  attr_accessible :title, :slug, :blurb,  :content
+
   validates :title, presence: true, length: { minimum: 5 }
   validates :slug, presence: true, format: { with: /\A[a-zA-Z-]+\z/}
   validates :content, presence: true, length: { minimum: 10 }
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
-  get 'pages/index'
+  root 'pages#index'
+
+  resources :pages
+
+  %w[about contact cv].each do |page|
+    get page, controller: 'pages', action: page
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
